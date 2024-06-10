8d97932f11be0efd0fbae7f8f7f16eeeaa332494
Sandudil <s.dilharadayananda@gmail.com>
2024-06-10 17:23:36 +0530

    Initial commit

---
+++ blogproj/blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Destiny Franks',
+        'title': 'Blog Post 1',
+        'content': 'This is my first blog post',
+        'date_posted': '7th May, 2024'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'This is my second blog post',
+        'date_posted': '14th May, 2024'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': "About Page"})
