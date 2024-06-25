2ab4c5a78b5a75b3161dcd30a80ec3c75982449f
Iyantha <amesharuchi99@gmail.com>
2024-06-25 14:57:58 +0530

    Update views.py

--- blogproj/blog/views.py
+++ blogproj/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+
+
+
+
+
 posts = [
     {
         'author': 'Destiny Franks',
