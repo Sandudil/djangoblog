edde0f0518097abf6947e1730401ba6fb7e7999c
Iyantha <amesharuchi99@gmail.com>
2024-06-25 15:25:59 +0530

    Update urls.py

--- blogproj/blog/urls.py
+++ blogproj/blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('about/', views.about, name="blog-about")
     path('', PostListView.as_view(), name="blog-home"),
     path('post-new/', PostCreateView.as_view(), name="blog-new"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="blog-detail"),
 
     
 ]
