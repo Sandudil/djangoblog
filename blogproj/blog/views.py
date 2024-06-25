8131ca94781389f521c0541848cb7700cd50c5cf
Iyantha <amesharuchi99@gmail.com>
2024-06-25 14:58:28 +0530

    Update views.py

--- blogproj/blog/views.py
+++ blogproj/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 
